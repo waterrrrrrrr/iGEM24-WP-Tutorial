a510f038466def8e4b5e90e2d04e57cbdf6a35ec
Jialan Yang <2695566358@qq.com>
2024-02-25 05:48:55 +0800

    soft

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+The docker container occupies 1.8GB
+The web app occupies approximately 44MB of running memory
+The  source file consists of 1000+ lines  https://github.com/Xspielberg/BioProtoType/tree/main
